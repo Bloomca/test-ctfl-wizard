b765fcfa776ba910671e1ff528e8fd96ef065403
Vsevolod Zaikov <seva.zaikov@gmail.com>
2018-09-06 18:10:19 +0200

    add wizard to the home page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {BrowserRouter as Router}  from 'react-router-dom';
-import ReactRouter from './router/router';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactRouter from "./router/router";
+import { init } from "contentful-wizard";
 
+init({
+  spaceId: "gwr2bryocotv",
+  key: "1b084e7585e640e51efed3f4173a6a06070ed0cc96a220fbb3b794edb721339b",
+  preview: true
+});
 
-
-
-
-
-
-ReactDOM.render(<Router>
-
-    <ReactRouter/>
-</Router>
-    ,
-    document.getElementById('root'));
-
+ReactDOM.render(
+  <Router>
+    <ReactRouter />
+  </Router>,
+  document.getElementById("root")
+);
